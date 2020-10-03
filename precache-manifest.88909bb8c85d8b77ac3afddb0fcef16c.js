5a375046b35dcd59c6a864e10ffffadf65fa2bc4
Dmitry Kalko <master-denn@mail.ru>
2020-10-03 17:05:18 +0300

    Updates

---
+++ precache-manifest.88909bb8c85d8b77ac3afddb0fcef16c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61869b76c94b20ecb9528eafef85abeb",
+    "url": "/sula/index.html"
+  },
+  {
+    "revision": "df4a9588b506b5c8c581",
+    "url": "/sula/static/css/main.22b8de4a.chunk.css"
+  },
+  {
+    "revision": "3a8671cfc4a9bd7f8149",
+    "url": "/sula/static/js/2.1de7bccd.chunk.js"
+  },
+  {
+    "revision": "cf0565a1cf8a621bfb906afd263c3a1b",
+    "url": "/sula/static/js/2.1de7bccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4a9588b506b5c8c581",
+    "url": "/sula/static/js/main.b26c8587.chunk.js"
+  },
+  {
+    "revision": "66aa047c05da758fa16e",
+    "url": "/sula/static/js/runtime-main.3a1b7025.js"
+  },
+  {
+    "revision": "f4b99cca2860c4c2e6ccfec1bdc6ee18",
+    "url": "/sula/static/media/logo.f4b99cca.png"
+  },
+  {
+    "revision": "8daccb5d93f2f35659bdddb8793bc9cb",
+    "url": "/sula/static/media/logoFooter.8daccb5d.png"
+  },
+  {
+    "revision": "a940ab03c12dd0246b98057b7d78e36e",
+    "url": "/sula/static/media/sertificate.a940ab03.png"
+  }
+]);
